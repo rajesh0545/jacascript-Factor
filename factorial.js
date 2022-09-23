62967e6180a7fecd0141cb820a8f598e124f5a31
rajesh <krajeshgowri7032@mail.com>
2022-09-23 14:37:23 +0530

    add files

---
+++ factorial.js
@@ -0,0 +1,30 @@
+// const x= 3;
+
+// let fact=1;
+// if(x<0){
+//     console.log('error');
+// }
+// else if(x===0){
+//     console.log('the factoral of ${x} is 1');
+
+// }
+// else{
+    
+//     for(i=1; i<=x; i++){
+//         fact *=i;
+//     }
+
+// }
+// console.log(`this factorial of${x} is a ${fact}`);
+
+
+//Factoral positive number
+
+const num=10;
+
+for (i=1; i<=num; i++){
+    if(num % i==0){
+        console.log(i)
+    }
+}
+
